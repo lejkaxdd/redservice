e30114d9341cb86f6c2ba8e5364f7cc4dc825775
lejkaxdd <lejkaxdd@yandex.ru>
2024-11-08 16:40:27 +0000

    change agent name

--- logger.py
+++ logger.py
@@ -17,7 +17,7 @@ log.basicConfig(filename='./log/app.log', level=log.INFO, format='%(asctime)s - 
 # 1006 - Attempt to exploit SQL Injection
 
 global loga
-loga = '''{{"event_code" : "{event}", "agent" : {{"ip" : "10.61.10.11", "name": "wazuh-agent", "id" : "002"}}, "data": {{"app_proto": "http", "src_ip": "{ip}", "src_port": "{port}", "dest_port": "5000", "event_timestamp" : "{time}"}}, "http": {{"hostname": "{hostname}", "protocol": "{protocol}", "http_method": "{http_method}", "payload" : ["{payload}"], "url": "{url}", "http_user_agent": "{http_user_agent}", "status": "{status}"}}, "err_message" : "{error}"}}'''
+loga = '''{{"event_code" : "{event}", "agent" : {{"ip" : "10.61.10.11", "name": "RedService", "id" : "002"}}, "data": {{"app_proto": "http", "src_ip": "{ip}", "src_port": "{port}", "dest_port": "5000", "event_timestamp" : "{time}"}}, "http": {{"hostname": "{hostname}", "protocol": "{protocol}", "http_method": "{http_method}", "payload" : ["{payload}"], "url": "{url}", "http_user_agent": "{http_user_agent}", "status": "{status}"}}, "err_message" : "{error}"}}'''
 
 def start_page(flag, params):
     output = loga.format(event=params[0], ip=params[1], port=params[2], time=params[3], hostname=params[4], protocol=params[5],
